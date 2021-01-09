ab735c0a657335f77c3afa75fcd7b52f729db28e
Валерия <v.valvachyova@gmail.com>
2021-01-09 16:26:43 +0300

    Changes: change jUnit to TestNG for parametrized tests

--- src/test/java/com/testtask/ClassFinderTest.java
+++ src/test/java/com/testtask/ClassFinderTest.java
@@ -1,70 +1,48 @@
 package com.testtask;
 
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.testtask.ClassFinder.findClasses;
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class ClassFinderTest {
 
-    private final String FILEPATH = "./src/test/resources/classes.txt";
+    @DataProvider(name = "test-class-finder")
+    Object[][] dataProviderTest() {
+        String filepath = "./src/test/resources/classes.txt";
+        return new Object[][]{
+                //символы в шаблоне в разном регистре (положительный сценарий)
+                {filepath, "FoBa", new ArrayList<>(Arrays.asList("c.d.FooBar", "a.b.FooBarBaz"))},
+                //символы в шаблоне в верхнем регистре в некорректном порядке
+                {filepath, "BF", new ArrayList<>()},
+                //символы в шаблоне в нижнем регистре (регистронезависимый поиск)
+                {filepath, "fbb", new ArrayList<>(Collections.singletonList("a.b.FooBarBaz"))},
+                //символы в шаблоне в разном регистре (негативный сценарий)
+                {filepath, "fBb", new ArrayList<>()},
+                //шаблон оканчивается символом ' '
+                {filepath, "FBar ", new ArrayList<>(Collections.singletonList("c.d.FooBar"))},
+                //шаблон подразумевает пропуск символов (*)
+                {filepath, "B*rBaz", new ArrayList<>(Collections.singletonList("a.b.FooBarBaz"))},
+                //проверка сортировки в результатирующем списке
+                {"./src/test/resources/classesForSortCheck.txt", "FooBar",
+                        new ArrayList<>(Arrays.asList("e.f.aFooBar", "c.d.bFooBar", "a.b.bFooBar", "a.b.zFooBarBaz"))}
+        };
+    }
 
-    @Test(expected = IOException.class)
+    //проверка случая, когда введен некорректный путь к файлу
+    @Test(expectedExceptions = IOException.class)
     public void checkIncorrectFile() throws IOException {
         findClasses("test.txt", "FoBa");
     }
 
-    @Test
-    public void checkCamelCaseLetters() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        findResult.add("c.d.FooBar");
-        findResult.add("a.b.FooBarBaz");
-        assertEquals(findResult, findClasses(FILEPATH, "FoBa"));
-    }
-
-    @Test
-    public void checkUpperCaseWrongOrder() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        assertEquals(findResult, findClasses(FILEPATH, "BF"));
-    }
-
-    @Test
-    public void checkCaseInsensitive() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        findResult.add("a.b.FooBarBaz");
-        assertEquals(findResult, findClasses(FILEPATH, "fbb"));
-    }
-
-    @Test
-    public void checkCaseSensitive() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        assertEquals(findResult, findClasses(FILEPATH, "fBb"));
-    }
-
-    @Test
-    public void checkEndsWithSpace() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        findResult.add("c.d.FooBar");
-        assertEquals(findResult, findClasses(FILEPATH, "FBar "));
-    }
-
-    @Test
-    public void checkMissingLetters() throws IOException {
-        ArrayList<String> findResult = new ArrayList<>();
-        findResult.add("a.b.FooBarBaz");
-        assertEquals(findResult, findClasses(FILEPATH, "B*rBaz"));
-    }
-
-    @Test
-    public void checkSort() throws Exception {
-        ArrayList<String> findResult = new ArrayList<>();
-        findResult.add("e.f.aFooBar");
-        findResult.add("c.d.bFooBar");
-        findResult.add("a.b.bFooBar");
-        findResult.add("a.b.zFooBarBaz");
-        assertEquals(findResult, findClasses("./src/test/resources/classesForSortCheck.txt", "FooBar"));
+    @Test(dataProvider = "test-class-finder")
+    public void checkCamelCaseLetters(String filepath, String pattern, ArrayList<String> findResult) throws IOException {
+        assertEquals(findResult, findClasses(filepath, pattern));
     }
 }
